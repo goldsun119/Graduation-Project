ccec62c7003dac6d907613d3f0383cbaf165d5b7
김하은 <kim_he0311@hanmail.net>
2019-04-08 19:24:54 +0900

    unity Project file
    유니티 내부 콜라보레이션으로 공유하다 깃허브로 옮김

---
+++ FineDust/Assets/Scripts/CameraMgr.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMgr : MonoBehaviour
+{
+    public Camera mainCam;     // 3인칭
+    public Camera subCam;       // 1인칭
+
+    public bool isCamMain = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MainCamOn();
+    }
+
+    public void MainCamOn()
+    {
+        mainCam.enabled = true;
+        subCam.enabled = false;
+    }
+
+    public void SubCamOn()
+    {
+        mainCam.enabled = false;
+        subCam.enabled = true;
+    }
+}
---
+++ FineDust/Assets/Scripts/Ctrl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ctrl : MonoBehaviour
+{
+
+    CameraMgr camMgr;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        camMgr = GetComponent<CameraMgr>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyUp(KeyCode.V))
+        {
+            if (camMgr.isCamMain) { camMgr.SubCamOn(); camMgr.isCamMain = false; }
+            else { camMgr.MainCamOn(); camMgr.isCamMain = true; }
+        }
+
+    }
+}
