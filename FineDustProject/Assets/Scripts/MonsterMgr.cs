dc45388af9870cfceaa4d03b86fcf9ddd4ba374c
duswl8811 <duswlsp@naver.com>
2019-05-08 03:18:09 +0900

    몬스터 맞았을 때, 반응추가 + 플레이어 추적 오류 수정
    몬스터가 피격 됐을 때, 조금 밀린 뒤 그자리에 멈춰서 있도록 하게 했습니다.
    그리고 플레이어 추적 반경을 늘려도 그 범위 내에서 몬스터가 플레이어를 추적하지 못하는 오류가 있었는데, getobject player가 아니라 player의 transform을 받도록 수정 해주고 해결되었습니다.

--- FineDustProject/Assets/Scripts/MonsterMgr.cs
+++ FineDustProject/Assets/Scripts/MonsterMgr.cs
@@ -5,31 +5,38 @@ using UnityEngine.AI;
 
 public class MonsterMgr : MonoBehaviour
 {
-    GameObject player;
+    public Transform player_tf;
     NavMeshAgent nav;
 
     public int HP;
     public bool isCollision = false;
+    public bool is_KnockBack = false;
+    public bool is_Wait = false;
 
-    public float targetRange = 5.0f;
+    public float targetRange = 50.0f;
+    public float KnockBack_Time = 0.1f;
+    public float Waiting_Time = 0.0f;
 
     void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player");
+        player_tf = GameObject.FindGameObjectWithTag("Player").transform;
         nav = GetComponent<NavMeshAgent>();
         nav.speed = 1.0f;
-
+        targetRange = 50.0f;
         HP = 100;
     }
 
     // Update is called once per frame
     void Update()
     {
+        player_tf = GameObject.FindGameObjectWithTag("Player").transform;
+
         // 몬스터 플레이어 따라가기
-        if (CheckDistance())
+        if ( (Vector3.Distance(player_tf.position, transform.position) <= targetRange) && (!is_Wait))
         {
-            nav.SetDestination(player.transform.position);
-            Debug.Log(player.transform.position);
+            nav.SetDestination(player_tf.position);
+            float distance = Vector3.Distance(player_tf.position, transform.position);
+            Debug.Log("몬스터와 캐릭터 사이의 거리" + distance);
         }
         else
         {
@@ -43,8 +50,29 @@ public class MonsterMgr : MonoBehaviour
             {
                 HP -= 10;
                 Debug.Log("피 " + HP + "만큼 남아따");
+                is_KnockBack = true;
+                KnockBack_Time = 0.1f;
             }
         }
+
+        if (is_KnockBack)
+        {
+            transform.Translate((Vector3.back) * KnockBack_Time);
+            KnockBack_Time += Time.deltaTime;
+            if (KnockBack_Time > 0.3)
+            {
+                is_KnockBack = false;
+                is_Wait = true;
+                Waiting_Time = 0.0f;
+            }
+        }
+
+        if (is_Wait)
+        {
+            Waiting_Time += Time.deltaTime;
+            if (Waiting_Time > 0.5) is_Wait = false;
+        }
+
 
         if (HP == 0)
         {
@@ -62,12 +90,12 @@ public class MonsterMgr : MonoBehaviour
         }
     }
 
-    bool CheckDistance()
-    {
-        float distance = player.transform.position.magnitude;
+    //bool CheckDistance()
+    //{
+    //    float distance = Vector3.Distance(player.transform.position, transform.position);
+    //    Debug.Log("몬스터와 캐릭터 사이의 거리" + distance);
+    //    if (distance <= targetRange) return true;
 
-        if (distance <= targetRange) return true;
-
-        return false;
-    }
+    //    return false;
+    //}
 }
