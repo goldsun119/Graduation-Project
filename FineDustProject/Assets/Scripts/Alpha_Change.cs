3a071ab20b9d7115429776c190267d28d6835cd2
duswl8811 <duswlsp@naver.com>
2019-08-10 08:12:53 +0900

    LOD 처리 성공, 새로운 텍스처 및 오브젝트 추가, 반투명 shader
    shader를 통해 플레이어를 가리는 것들을 반투명하게 만드려고 합니다. 그러나 그냥  mesh render를 가지고 있는 오브젝트에는 적용이 되지만 다른 것들에는 잘 적용이 안됩니다. 또한 material이 2개 이상인 것에도 적용이 안됩니다.

---
+++ FineDustProject/Assets/Scripts/Alpha_Change.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Alpha_Change : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.name + "가려져라" );
+        other.GetComponent<MeshRenderer>().material.color = new Color(0f, 0f, 0f, 0.3f);
+    }
+}
